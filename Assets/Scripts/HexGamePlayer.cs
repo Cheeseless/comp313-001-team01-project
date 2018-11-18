1b22ab864af2c34d5e3f407a44355e24990b90fd
Pedro Bento <pedrobettt@gmail.com>
2018-11-18 14:36:45 -0500

    Added testing Scene and testing version of Map Editor

---
+++ Assets/Scripts/HexGamePlayer.cs
@@ -0,0 +1,307 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexGamePlayer : MonoBehaviour {
+
+    int activeElevation;
+
+    int activeTerrainTypeIndex;
+
+    int activeUrbanLevel, activeFarmLevel, activePlantLevel, activeSpecialIndex;
+    int activeWaterLevel;
+
+    bool applyElevation = true;
+
+    bool applyUrbanLevel, applyFarmLevel, applyPlantLevel, applySpecialIndex;
+    bool applyWaterLevel = true;
+
+    int brushSize;
+    HexDirection dragDirection;
+
+    public HexGrid hexGrid;
+    public GameManager gameManager;
+
+    bool isDrag;
+
+    Player owner;
+    HexCell previousCell;
+
+    OptionalToggle riverMode, roadMode, walledMode;
+
+    public Material terrainMaterial;
+
+    public void SetOwner(int index) {
+        owner = gameManager.GetPlayer(index);
+    }
+
+    public void SetTerrainTypeIndex(int index) {
+        activeTerrainTypeIndex = index;
+    }
+
+    public void SetApplyElevation(bool toggle) {
+        applyElevation = toggle;
+    }
+
+    public void SetElevation(float elevation) {
+        activeElevation = (int) elevation;
+    }
+
+    public void SetApplyWaterLevel(bool toggle) {
+        applyWaterLevel = toggle;
+    }
+
+    public void SetWaterLevel(float level) {
+        activeWaterLevel = (int) level;
+    }
+
+    public void SetApplyUrbanLevel(bool toggle) {
+        applyUrbanLevel = toggle;
+    }
+
+    public void SetUrbanLevel(float level) {
+        activeUrbanLevel = (int) level;
+    }
+
+    public void SetApplyFarmLevel(bool toggle) {
+        applyFarmLevel = toggle;
+    }
+
+    public void SetFarmLevel(float level) {
+        activeFarmLevel = (int) level;
+    }
+
+    public void SetApplyPlantLevel(bool toggle) {
+        applyPlantLevel = toggle;
+    }
+
+    public void SetPlantLevel(float level) {
+        activePlantLevel = (int) level;
+    }
+
+    public void SetApplySpecialIndex(bool toggle) {
+        applySpecialIndex = toggle;
+    }
+
+    public void SetSpecialIndex(float index) {
+        activeSpecialIndex = (int) index;
+    }
+
+    public void SetBrushSize(float size) {
+        brushSize = (int) size;
+    }
+
+    public void SetRiverMode(int mode) {
+        riverMode = (OptionalToggle) mode;
+    }
+
+    public void SetRoadMode(int mode) {
+        roadMode = (OptionalToggle) mode;
+    }
+
+    public void SetWalledMode(int mode) {
+        walledMode = (OptionalToggle) mode;
+    }
+
+    public void SetEditMode(bool toggle) {
+        enabled = toggle;
+    }
+
+    public void ShowGrid(bool visible) {
+        if (visible) {
+            terrainMaterial.EnableKeyword("GRID_ON");
+        }
+        else {
+            terrainMaterial.DisableKeyword("GRID_ON");
+        }
+    }
+
+    void Awake() {
+        terrainMaterial.EnableKeyword("GRID_ON");
+        
+        SetEditMode(true);
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            Debug.Log("EditMode is On");
+            SetEditMode(true);
+        }
+
+        if (!EventSystem.current.IsPointerOverGameObject()) {
+            if (Input.GetMouseButton(0)) {
+                HandleInput();
+                return;
+            }
+            
+
+            if (Input.GetKey(KeyCode.LeftControl)) {
+                Debug.Log("Building Units");
+                if (Input.GetKey(KeyCode.Mouse0)) {
+                    DestroyUnit();
+                    Debug.Log("You tried to kill me");
+                }
+                else if (Input.GetKey(KeyCode.Mouse1)){
+                    Debug.Log("You have created LIFE");
+                    CreateUnit();
+                   
+                }
+
+                return;
+            }
+        }
+
+        previousCell = null;
+    }
+
+    HexCell GetCellUnderCursor() {
+        return
+            hexGrid.GetCell(Camera.main.ScreenPointToRay(Input.mousePosition));
+    }
+
+    void CreateUnit() {
+        HexCell cell = GetCellUnderCursor();
+        if (cell && !cell.Unit) {
+            GameUnit unit = Instantiate(HexUnit.unitPrefab).GetComponent<GameUnit>();
+            unit.Owner = gameManager.GetPlayerIndex(owner);
+            /*switch (owner) {
+                case 0:
+                    unit.GetComponentInChildren<Renderer>().material.color = Color.blue;
+                    break;
+                case 1:
+                    unit.GetComponentInChildren<Renderer>().material.color = Color.red;
+                    break;
+                case 2:
+                    unit.GetComponentInChildren<Renderer>().material.color = Color.green;
+                    break;
+                case 3:
+                    unit.GetComponentInChildren<Renderer>().material.color = Color.black;
+                    break;
+            }
+            */
+            hexGrid.AddUnit(unit.HexUnit, cell, Random.Range(0f, 360f));
+        }
+    }
+
+    void DestroyUnit() {
+        HexCell cell = GetCellUnderCursor();
+        if (cell && cell.Unit) {
+            hexGrid.RemoveUnit(cell.Unit.HexUnit);
+        }
+    }
+
+    void HandleInput() {
+        HexCell currentCell = GetCellUnderCursor();
+        if (currentCell) {
+            if (previousCell && previousCell != currentCell) {
+                ValidateDrag(currentCell);
+            }
+            else {
+                isDrag = false;
+            }
+
+            EditCells(currentCell);
+            previousCell = currentCell;
+        }
+        else {
+            previousCell = null;
+        }
+    }
+
+    void ValidateDrag(HexCell currentCell) {
+        for (
+            dragDirection = HexDirection.NE;
+            dragDirection <= HexDirection.NW;
+            dragDirection++
+        ) {
+            if (previousCell.GetNeighbor(dragDirection) == currentCell) {
+                isDrag = true;
+                return;
+            }
+        }
+
+        isDrag = false;
+    }
+
+    void EditCells(HexCell center) {
+        int centerX = center.coordinates.X;
+        int centerZ = center.coordinates.Z;
+
+        for (int r = 0, z = centerZ - brushSize; z <= centerZ; z++, r++) {
+            for (int x = centerX - r; x <= centerX + brushSize; x++) {
+                EditCell(hexGrid.GetCell(new HexCoordinates(x, z)));
+            }
+        }
+
+        for (int r = 0, z = centerZ + brushSize; z > centerZ; z--, r++) {
+            for (int x = centerX - brushSize; x <= centerX + r; x++) {
+                EditCell(hexGrid.GetCell(new HexCoordinates(x, z)));
+            }
+        }
+    }
+
+    void EditCell(HexCell cell) {
+        if (cell) {
+            if (activeTerrainTypeIndex >= 0) {
+                cell.TerrainTypeIndex = activeTerrainTypeIndex;
+            }
+
+            if (applyElevation) {
+                cell.Elevation = activeElevation;
+            }
+
+            if (applyWaterLevel) {
+                cell.WaterLevel = activeWaterLevel;
+            }
+
+            if (applySpecialIndex) {
+                cell.SpecialIndex = activeSpecialIndex;
+            }
+
+            if (applyUrbanLevel) {
+                cell.UrbanLevel = activeUrbanLevel;
+            }
+
+            if (applyFarmLevel) {
+                cell.FarmLevel = activeFarmLevel;
+            }
+
+            if (applyPlantLevel) {
+                cell.PlantLevel = activePlantLevel;
+            }
+
+            if (riverMode == OptionalToggle.No) {
+                cell.RemoveRiver();
+            }
+
+            if (roadMode == OptionalToggle.No) {
+                cell.RemoveRoads();
+            }
+
+            if (walledMode != OptionalToggle.Ignore) {
+                cell.Walled = walledMode == OptionalToggle.Yes;
+            }
+
+            if (isDrag) {
+                HexCell otherCell = cell.GetNeighbor(dragDirection.Opposite());
+                if (otherCell) {
+                    if (riverMode == OptionalToggle.Yes) {
+                        otherCell.SetOutgoingRiver(dragDirection);
+                    }
+
+                    if (roadMode == OptionalToggle.Yes) {
+                        otherCell.AddRoad(dragDirection);
+                    }
+                }
+            }
+        }
+    }
+
+    enum OptionalToggle {
+
+        Ignore,
+        Yes,
+        No
+
+    }
+
+}
