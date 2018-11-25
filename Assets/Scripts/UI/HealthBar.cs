e30aac88a55adbe3a68b995c5a517aa9354a7fb9
Andre Passos <stepsup@outlook.com>
2018-11-24 19:11:39 -0500

    Script for the Healthbar - Damage, Heal, FullHeal

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    private Slider slider;
+
+    // Use this for initialization
+    void Start () {
+        slider = gameObject.GetComponentInChildren<Slider>();
+        this.Damage(200, 400);
+        Debug.Log(slider.value);
+        this.Heal(100, 400);
+        Debug.Log(slider.value);
+    }
+
+    public void FullHeal()
+    {
+        slider.value = 100;
+
+    }
+
+    public void Heal(float amount, float maxHealth)
+    {
+        float heal;
+        heal = (100 * amount) / maxHealth;
+        slider.value = slider.value + heal;
+
+    }
+
+    public void Damage(float amount, float maxHealth)
+    {
+        float damage;
+        damage = (100 * amount) / maxHealth;
+        slider.value = slider.value - damage;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
