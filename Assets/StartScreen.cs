a76e517bf80daf5e8e681a0b5b4dfe2a8f2f8be5
Andre Passos <stepsup@outlook.com>
2018-12-03 13:27:50 -0500

    Level Selection added

---
+++ Assets/StartScreen.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour {
+    
+    private GameObject mainCanvas;
+    private GameObject levelSelectionCanvas;
+    private GameObject startText;
+    private AudioSource buttonSound;
+    private float myTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+        mainCanvas = GameObject.Find("MainCanvas");
+        levelSelectionCanvas = GameObject.Find("LevelSelectionCanvas");
+        startText = GameObject.Find("StartText");
+        levelSelectionCanvas.SetActive(false);
+        startText.SetActive(false);
+        buttonSound = this.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myTime = myTime + Time.deltaTime;
+        if(myTime > 2.5f)
+        {
+            startText.SetActive(true);
+        }
+
+        if (Input.anyKey)
+        {
+            buttonSound.Play();
+            mainCanvas.SetActive(false);
+            levelSelectionCanvas.SetActive(true);
+        }
+    }
+
+    public void SelectLevel(string sceneName)
+    {
+        buttonSound.Play();
+        SceneManager.LoadScene(sceneName);
+    }
+    
+}
